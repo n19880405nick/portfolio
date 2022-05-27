85a2d0ab13ed0540a188e45f2483c444c05777a0
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-05-27 04:34:27 +0000

    [Finish]admin layout
    -mchange font, header design, body back-ground

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,23 @@ class ApplicationController < ActionController::Base
 
 
 	# 管理者、顧客それぞれのログイン後の表示画面変更
-	# def after_sign_in_path_for(resource_or_scope)
-		# if resource_or_scope.is_a?(Admin)
-			# admin_top_path
-		# else
-			# root_path
-		# end
-	# end
+	def after_sign_in_path_for(resource_or_scope)
+		if resource_or_scope.is_a?(Admin)
+			admin_top_path
+		else
+			root_path
+		end
+	end
 
 	# 管理者、顧客それぞれのログアウト後の表示画面変更
-	# def after_sign_out_path_for(resource_or_scope)
-		# if resource_or_scope == :admin
-			# admin_session_path
-		# else
-	# root_path
-		# end
-	# end
+	def after_sign_out_path_for(resource_or_scope)
+		if resource_or_scope == :admin
+			admin_session_path
+		else
+			root_path
+		end
+	end
+
 	before_action :get_latest_tag
 
 	def get_latest_tag
