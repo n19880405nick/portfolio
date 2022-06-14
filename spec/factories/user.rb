09508c85567bf55a9dd49c07beaccc45aa7ccca3
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-06-14 04:11:11 +0000

    rspec making

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.name}
     email{Faker::Internet.email}
-    password{Faker::Lorem.characters(number:10)}
+    password{'password'}
+    password_confirmation {'password'}
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -18,6 +18,43 @@ describe 'ユーザー登録' do
       it 'ヘッダに「ログイン」へのリンクが表示されているか' do
         expect(page).to have_link "ログイン", href: new_user_session_path
       end
+      it '「投稿一覧」へのリンクが表示されているか' do
+        expect(page).to have_link "", href: posts_path
+      end
+    end
+  end
+  describe '新規登録画面のテスト' do
+    before do
+      visit new_user_registration_path
+    end
+    context '表示の確認' do
+      it '名前入力欄が表示されているか' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'メールアドレス入力欄が表示されているか' do
+        expect(page).to have_field 'user[email]'
+      end
+      it 'パスワード入力欄が表示されているか' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'パスワード確認入力欄が表示されているか' do
+        expect(page).to have_field 'user[password_confirmation]'
+      end
+    end
+    context '新規登録処理に関するテスト' do
+      before do
+        fill_in 'user[name]', with: Faker::Name.name
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+      end
+      it 'ユーザーとして登録されるか' do
+        expect{click_button '新規登録する'}.to change(User.all, :count).by(1)
+      end
+      it '登録後のリダイレクト先がマイページになっているか' do
+        click_button '新規登録する'
+        expect(current_path).to eq my_page_path
+      end
     end
   end
 end
