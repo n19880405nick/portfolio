02670e1cab83ccbbda1837fb0fd973a4a4850eb8
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-06-22 09:22:33 +0000

    [Finish]rspec
    -mmake users, posts, admin

--- spec/system/admins_spec.rb
+++ spec/system/admins_spec.rb
@@ -72,11 +72,81 @@ describe '管理者' do
     end
   end
   describe 'ユーザー一覧画面のテスト' do
+    before do
+      sign_in admin
+      visit admin_users_path
+    end
+    context '表示の確認' do
+      it 'ユーザー氏名が表示され、それが「ユーザー詳細画面」リンクになっているか' do
+        expect(page).to have_link user.name, href: admin_user_path(user)
+      end
+      it 'ユーザーステータスに「有効」が表示されているか' do
+        expect(page).to have_content '有効'
+      end
+    end
   end
   describe 'ユーザー詳細画面のテスト' do
+    before do
+      sign_in admin
+      visit admin_user_path(user)
+    end
+    context '表示の確認' do
+      it 'ユーザー氏名が表示されているか' do
+        expect(page).to have_content user.name
+      end
+      it 'ユーザーのアドレスが表示されているか' do
+        expect(page).to have_content user.email
+      end
+      it 'ユーザーステータスに「有効」が表示されているか' do
+        expect(page).to have_content '有効'
+      end
+      it '「ユーザー編集画面」へのリンクが表示されているか' do
+        expect(page).to have_link '', href: edit_admin_user_path(user)
+      end
+    end
   end
   describe 'ユーザー編集画面のテスト' do
+    before do
+      sign_in admin
+      visit edit_admin_user_path(user)
+    end
+    context '表示の確認' do
+      it 'ユーザー名編集欄が表示されているか' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'ユーザーアドレス編集欄が表示されているか' do
+        expect(page).to have_field 'user[email]'
+      end
+      it 'ユーザーステータス変更ラジオボタンが表示されているか（有効が選択されているか）' do
+        expect(page).to have_checked_field('有効')
+      end
+    end
+    context '変更処理のテスト' do
+      it 'ユーザーステータスを退会に変更できるか' do
+        choose '退会'
+        click_on '編集を保存する'
+        expect(page).to have_content '退会'
+      end
+    end
   end
   describe '投稿詳細画面のテスト' do
+    before do
+      sign_in admin
+      visit admin_post_path(post)
+    end
+    context '表示の確認' do
+      it '「投稿を削除」ボタンが表示されているか' do
+        expect(page).to have_link '投稿を削除', href: admin_post_path(post)
+      end
+      it '投稿が表示されているか' do
+        expect(page).to have_content post.title
+      end
+      it 'コメントが表示されているか' do
+        expect(page).to have_content comment.comment
+      end
+      it '「コメント削除」ボタンが表示されているか' do
+        expect(page).to have_link 'コメント削除', href: admin_post_comment_path(post, comment)
+      end
+    end
   end
 end
